b072c0543f02effe87f5cc44949dd11d81fa9caa
GFR <bengallme@gmail.com>
2020-08-30 14:22:15 -0500

    added a new component

--- src/ColorDisplay.js
+++ src/ColorDisplay.js
@@ -2,16 +2,36 @@ import React from "react";
 import "./ColorDisplay.css";
 import ColorCard from "./ColorCard";
 
-const cardData = {
-  name: "Panthone blue",
-  colors: ["#2B04E8", "#4A40FF", "#5009FF", "#6A12E8", "#8B09FF"],
-};
+const cardData = [
+  {
+    name: "Panthone blue",
+    colors: ["#2B04E8", "#4A40FF", "#5009FF", "#6A12E8", "#8B09FF"],
+  },
+  {
+    name: "Royal dance",
+    colors: ["#40007F"],
+  },
+  {
+    name: "Panthone blue",
+    colors: ["#2B04E8", "#4A40FF", "#5009FF", "#6A12E8", "#8B09FF"],
+  },
+  {
+    name: "Royal dance",
+    colors: ["#40007F", "#6000BF", "#8000FF", "#200040", "#7300E5"],
+  },
+  {
+    name: "Royal dance",
+    colors: ["#40007F", "#6000BF", "#8000FF", "#200040", "#7300E5"],
+  },
+];
 
 function ColorDisplay() {
   return (
     <div className="container-fluid">
       <div className="row">
-        <ColorCard cardData={cardData} />
+        {cardData.map((cardData) => {
+          return <ColorCard cardData={cardData} />;
+        })}
       </div>
     </div>
   );
--- src/Container.js
+++ src/Container.js
@@ -1,12 +1,36 @@
 import React from "react";
 import Navbar from "./Navbar";
 import ColorDisplay from "./ColorDisplay";
+import ColorEditor from "./ColorEditor";
+const cardData = [
+  {
+    name: "Panthone blue",
+    colors: ["#2B04E8", "#4A40FF", "#5009FF", "#6A12E8", "#8B09FF"],
+  },
+  {
+    name: "Royal dance",
+    colors: ["#40007F"],
+  },
+  {
+    name: "Panthone blue",
+    colors: ["#2B04E8", "#4A40FF", "#5009FF", "#6A12E8", "#8B09FF"],
+  },
+  {
+    name: "Royal dance",
+    colors: ["#40007F", "#6000BF", "#8000FF", "#200040", "#7300E5"],
+  },
+  {
+    name: "Royal dance",
+    colors: ["#40007F", "#6000BF", "#8000FF", "#200040", "#7300E5"],
+  },
+];
 
 function Container() {
   return (
     <div>
       <Navbar />
-      <ColorDisplay />
+      {/* <ColorDisplay /> */}
+      <ColorEditor colorInfo={cardData[0]} />
     </div>
   );
 }
